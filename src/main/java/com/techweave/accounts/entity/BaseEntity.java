2a2b34ab9ffdc584e7a94af2ca243b8bb18e92ce
Monir <monir198@gmail.com>
2024-08-17 13:13:07 +0600

    added base entity as superclass

--- src/main/java/com/techweave/accounts/entity/BaseEntity.java
+++ src/main/java/com/techweave/accounts/entity/BaseEntity.java
@@ -1,4 +1,26 @@
 package com.techweave.accounts.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+import java.time.LocalDateTime;
+
+@MappedSuperclass       //tells spring data jpa that it is the base of all entity class
+@Getter
+@Setter
+@ToString
 public class BaseEntity {
+
+    @Column(updatable = false)
+    private LocalDateTime createdAt;
+    @Column(updatable = false)
+    private String createdBy;
+    @Column(insertable = false)
+    private LocalDateTime updatedAt;
+    @Column(insertable = false)
+    private String updatedBy;
+
 }
